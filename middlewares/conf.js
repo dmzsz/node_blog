fc09fe41417e2e95470885eb9a99aca7a170d2c3
康元 <kangyuan@kangyuandeMacBook-Pro.local>
2016-07-23 19:05:13 +0800

    注释clientID判断

--- middlewares/conf.js
+++ middlewares/conf.js
@@ -1,8 +1,8 @@
 var config = require('../config');
 
 exports.github = function (req, res, next) {
-  if (config.GITHUB_OAUTH.clientID === '10bc2d61fd3052100f04') {
-    return res.send('call the admin to set github oauth.');
-  }
+  // if (config.GITHUB_OAUTH.clientID === '10bc2d61fd3052100f04') {
+  //   return res.send('call the admin to set github oauth.');
+  // }
   next();
 };
